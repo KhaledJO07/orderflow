2a6f2beda1e2cb9c9a7bdb02165a8c962c298af6
Khaled Jokhadar <khaledjokhadar07@gmail.com>
2026-04-15 02:47:30 +0300

    fix the extract_oltp file

--- data_pipeline/extract/extract_oltp.py
+++ data_pipeline/extract/extract_oltp.py
@@ -1,23 +1,16 @@
-import psycopg2
-import pandas as pd
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
 import os
+import pandas as pd
 
-load_dotenv()
-
-def get_oltp_connection():
-    return psycopg2.connect(
-        host=os.getenv("OLTP_DB_HOST"),
-        port=os.getenv("OLTP_DB_PORT"),
-        database=os.getenv("OLTP_DB_NAME"),
-        user=os.getenv("OLTP_DB_USER"),
-        password=os.getenv("OLTP_DB_PASSWORD")
+def get_oltp_engine():
+    return create_engine(
+        f"postgresql://{os.getenv('OLTP_DB_USER')}:{os.getenv('OLTP_DB_PASSWORD')}@"
+        f"{os.getenv('OLTP_DB_HOST')}:{os.getenv('OLTP_DB_PORT')}/{os.getenv('OLTP_DB_NAME')}"
     )
 
 
 def extract_table(table_name):
-    conn = get_oltp_connection()
+    engine = get_oltp_engine()
     query = f"SELECT * FROM {table_name}"
-    df = pd.read_sql(query, conn)
-    conn.close()
+    df = pd.read_sql(query, engine)
     return df
